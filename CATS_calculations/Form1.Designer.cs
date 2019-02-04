520d87f3fd9488b164f72bca2167ab8618a6e944
Aleksey Krutov <alekseykrutovvv@gmail.com>
2019-02-04 18:40:26 +0300

    first commit

---
+++ CATS_calculations/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace CATS_calculations
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonForSelectFile = new System.Windows.Forms.Button();
+            this.ComboBoxForDates = new System.Windows.Forms.ComboBox();
+            this.ButtonForCalculatingCATS = new System.Windows.Forms.Button();
+            this.LabelForSelectFile = new System.Windows.Forms.Label();
+            this.LabelForFileLocation = new System.Windows.Forms.Label();
+            this.LabelForSelectDate = new System.Windows.Forms.Label();
+            this.LabelForCalculatedTime = new System.Windows.Forms.Label();
+            this.LabelTitleForCalcTime = new System.Windows.Forms.Label();
+            this.ButtonForCalculatingCATSweek = new System.Windows.Forms.Button();
+            this.LabelKrutov = new System.Windows.Forms.Label();
+            this.ButtonForServiceRequest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonForSelectFile
+            // 
+            this.buttonForSelectFile.Location = new System.Drawing.Point(57, 40);
+            this.buttonForSelectFile.Name = "buttonForSelectFile";
+            this.buttonForSelectFile.Size = new System.Drawing.Size(118, 29);
+            this.buttonForSelectFile.TabIndex = 0;
+            this.buttonForSelectFile.Text = "Выбрать файл...";
+            this.buttonForSelectFile.UseVisualStyleBackColor = true;
+            this.buttonForSelectFile.Click += new System.EventHandler(this.buttonForSelectFile_Click);
+            // 
+            // ComboBoxForDates
+            // 
+            this.ComboBoxForDates.FormattingEnabled = true;
+            this.ComboBoxForDates.Location = new System.Drawing.Point(57, 99);
+            this.ComboBoxForDates.Name = "ComboBoxForDates";
+            this.ComboBoxForDates.Size = new System.Drawing.Size(118, 21);
+            this.ComboBoxForDates.TabIndex = 1;
+            this.ComboBoxForDates.SelectedIndexChanged += new System.EventHandler(this.ComboBoxForDates_SelectedIndexChanged);
+            // 
+            // ButtonForCalculatingCATS
+            // 
+            this.ButtonForCalculatingCATS.Location = new System.Drawing.Point(57, 143);
+            this.ButtonForCalculatingCATS.Name = "ButtonForCalculatingCATS";
+            this.ButtonForCalculatingCATS.Size = new System.Drawing.Size(118, 37);
+            this.ButtonForCalculatingCATS.TabIndex = 2;
+            this.ButtonForCalculatingCATS.Text = "Рассчитать рабочее время за день";
+            this.ButtonForCalculatingCATS.UseVisualStyleBackColor = true;
+            this.ButtonForCalculatingCATS.Click += new System.EventHandler(this.ButtonForCalculatingCATS_Click);
+            // 
+            // LabelForSelectFile
+            // 
+            this.LabelForSelectFile.AutoSize = true;
+            this.LabelForSelectFile.Location = new System.Drawing.Point(57, 21);
+            this.LabelForSelectFile.Name = "LabelForSelectFile";
+            this.LabelForSelectFile.Size = new System.Drawing.Size(86, 13);
+            this.LabelForSelectFile.TabIndex = 3;
+            this.LabelForSelectFile.Text = "Выберете файл";
+            // 
+            // LabelForFileLocation
+            // 
+            this.LabelForFileLocation.AutoSize = true;
+            this.LabelForFileLocation.Location = new System.Drawing.Point(182, 55);
+            this.LabelForFileLocation.Name = "LabelForFileLocation";
+            this.LabelForFileLocation.Size = new System.Drawing.Size(0, 13);
+            this.LabelForFileLocation.TabIndex = 4;
+            // 
+            // LabelForSelectDate
+            // 
+            this.LabelForSelectDate.AutoSize = true;
+            this.LabelForSelectDate.Location = new System.Drawing.Point(57, 83);
+            this.LabelForSelectDate.Name = "LabelForSelectDate";
+            this.LabelForSelectDate.Size = new System.Drawing.Size(82, 13);
+            this.LabelForSelectDate.TabIndex = 5;
+            this.LabelForSelectDate.Text = "Выберете дату";
+            // 
+            // LabelForCalculatedTime
+            // 
+            this.LabelForCalculatedTime.AutoSize = true;
+            this.LabelForCalculatedTime.Location = new System.Drawing.Point(242, 195);
+            this.LabelForCalculatedTime.Name = "LabelForCalculatedTime";
+            this.LabelForCalculatedTime.Size = new System.Drawing.Size(0, 13);
+            this.LabelForCalculatedTime.TabIndex = 6;
+            this.LabelForCalculatedTime.Click += new System.EventHandler(this.LabelForCalculatedTime_Click);
+            this.LabelForCalculatedTime.MouseEnter += new System.EventHandler(this.LabelForCalculatedTime_MouseEnter);
+            // 
+            // LabelTitleForCalcTime
+            // 
+            this.LabelTitleForCalcTime.AutoSize = true;
+            this.LabelTitleForCalcTime.Location = new System.Drawing.Point(57, 195);
+            this.LabelTitleForCalcTime.Name = "LabelTitleForCalcTime";
+            this.LabelTitleForCalcTime.Size = new System.Drawing.Size(137, 13);
+            this.LabelTitleForCalcTime.TabIndex = 7;
+            this.LabelTitleForCalcTime.Text = "Итоговое рабочее время:";
+            // 
+            // ButtonForCalculatingCATSweek
+            // 
+            this.ButtonForCalculatingCATSweek.Location = new System.Drawing.Point(181, 143);
+            this.ButtonForCalculatingCATSweek.Name = "ButtonForCalculatingCATSweek";
+            this.ButtonForCalculatingCATSweek.Size = new System.Drawing.Size(118, 37);
+            this.ButtonForCalculatingCATSweek.TabIndex = 8;
+            this.ButtonForCalculatingCATSweek.Text = "Рассчитать рабочее время за неделю";
+            this.ButtonForCalculatingCATSweek.UseVisualStyleBackColor = true;
+            this.ButtonForCalculatingCATSweek.Click += new System.EventHandler(this.ButtonForCalculatingCATSweek_Click);
+            // 
+            // LabelKrutov
+            // 
+            this.LabelKrutov.AutoSize = true;
+            this.LabelKrutov.Location = new System.Drawing.Point(438, 195);
+            this.LabelKrutov.Name = "LabelKrutov";
+            this.LabelKrutov.Size = new System.Drawing.Size(0, 13);
+            this.LabelKrutov.TabIndex = 9;
+            // 
+            // ButtonForServiceRequest
+            // 
+            this.ButtonForServiceRequest.Location = new System.Drawing.Point(305, 143);
+            this.ButtonForServiceRequest.Name = "ButtonForServiceRequest";
+            this.ButtonForServiceRequest.Size = new System.Drawing.Size(133, 37);
+            this.ButtonForServiceRequest.TabIndex = 10;
+            this.ButtonForServiceRequest.Text = "Обращения выполненные за день";
+            this.ButtonForServiceRequest.UseVisualStyleBackColor = true;
+            this.ButtonForServiceRequest.Click += new System.EventHandler(this.ButtonForServiceRequest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 221);
+            this.Controls.Add(this.ButtonForServiceRequest);
+            this.Controls.Add(this.LabelKrutov);
+            this.Controls.Add(this.ButtonForCalculatingCATSweek);
+            this.Controls.Add(this.LabelTitleForCalcTime);
+            this.Controls.Add(this.LabelForCalculatedTime);
+            this.Controls.Add(this.LabelForSelectDate);
+            this.Controls.Add(this.LabelForFileLocation);
+            this.Controls.Add(this.LabelForSelectFile);
+            this.Controls.Add(this.ButtonForCalculatingCATS);
+            this.Controls.Add(this.ComboBoxForDates);
+            this.Controls.Add(this.buttonForSelectFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Расчет рабочего времени";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonForSelectFile;
+        private System.Windows.Forms.ComboBox ComboBoxForDates;
+        private System.Windows.Forms.Button ButtonForCalculatingCATS;
+        private System.Windows.Forms.Label LabelForSelectFile;
+        private System.Windows.Forms.Label LabelForFileLocation;
+        private System.Windows.Forms.Label LabelForSelectDate;
+        private System.Windows.Forms.Label LabelForCalculatedTime;
+        private System.Windows.Forms.Label LabelTitleForCalcTime;
+        private System.Windows.Forms.Button ButtonForCalculatingCATSweek;
+        private System.Windows.Forms.Label LabelKrutov;
+        private System.Windows.Forms.Button ButtonForServiceRequest;
+    }
+}
+
---
+++ CATS_calculations/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace CATS_calculations
+{
+    public partial class Form1 : Form
+    {
+        Excel.Application xlApp = new Excel.Application();
+
+        List<string> dates = new List<string>();
+        List<int> time = new List<int>();
+        string serviceRequests;
+        string selectedValueInCB;
+        int lastRow;
+        public Form1()
+        {
+            InitializeComponent();
+            ComboBoxForDates.DropDownStyle = ComboBoxStyle.DropDownList;
+            ComboBoxForDates.Enabled = false;
+            LabelTitleForCalcTime.Visible = false;
+            ButtonForCalculatingCATS.Enabled = false;
+            ButtonForCalculatingCATSweek.Enabled = false;
+            ButtonForServiceRequest.Enabled = false;
+            LabelKrutov.Text = "\u00A9 Krutov STAN-5000";
+        }
+        private void buttonForSelectFile_Click(object sender, EventArgs e)
+        {
+            xlApp.Quit();
+            ComboBoxForDates.Items.Clear();
+            dates.Clear();
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.Title = "Выберете Excel файл";
+            OFD.DefaultExt = "*.xls;*.xlsx";
+            OFD.Filter = "Microsoft Excel (*.xls*)|*.xls*";
+            if (OFD.ShowDialog() == DialogResult.OK && OFD.FileName.Length != 0)
+            {
+                LabelForFileLocation.Text = OFD.FileName;
+                LabelForCalculatedTime.Text = "";
+                LabelTitleForCalcTime.Visible = false;
+            }
+            else
+            {
+                return;
+            }
+            string xlFileName = OFD.FileName;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlSheet;
+            xlWorkBook = xlApp.Workbooks.Open(xlFileName);
+            xlSheet = xlWorkBook.Sheets[1];
+            lastRow = xlApp.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
+            GetDates(xlApp);
+            dates = dates.GroupBy(x => x).Select(y => y.FirstOrDefault()).ToList<string>();
+            dates.Sort();
+            dates.Reverse();
+            ComboBoxForDates.Items.AddRange(dates.ToArray<string>());
+            try
+            {
+                ComboBoxForDates.SelectedIndex = 0;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                MessageBox.Show("Скорее всего Вы выбрали не тот файл!");
+                LabelForFileLocation.Text = "";
+                LabelForCalculatedTime.Text = "";
+                LabelTitleForCalcTime.Visible = false;
+                ComboBoxForDates.Enabled = false;
+                ButtonForCalculatingCATS.Enabled = false;
+                ButtonForCalculatingCATSweek.Enabled = false;
+                ButtonForServiceRequest.Enabled = false;
+                return;
+            }
+            selectedValueInCB = dates[ComboBoxForDates.SelectedIndex];
+            ComboBoxForDates.Enabled = true;
+            ButtonForCalculatingCATS.Enabled = true;
+            ButtonForCalculatingCATSweek.Enabled = true;
+            ButtonForServiceRequest.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Excel.Application xlApp = new Excel.Application();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            xlApp.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlApp);
+        }
+        public void CalculateTime()
+        {
+            for (int i = 2; i <= lastRow; i++)
+            {
+                if (xlApp.Cells[i, 6] != null)
+                {
+                    time.Add(Convert.ToInt32((xlApp.Cells[i, 6].Value)));
+                }
+            }
+        }
+        public void GetDates(Excel.Application xlApp)
+        {
+            Regex regex = new Regex(@"\d\d.\d\d.\d\d\d\d");
+            for (int i = 2; i <= lastRow; i++)
+            {
+                if (xlApp.Cells[i, 3] != null)
+                {
+                    Match match = regex.Match((xlApp.Cells[i, 3].Value).ToString());
+                    dates.Add(match.Value.ToString());
+                }
+            }
+        }
+        public void GetServiceRequests()
+        {
+            serviceRequests = "Выполнение обращений SR - ";
+            for (int i = 2; i <= lastRow; i++)
+            {
+                string s = (xlApp.Cells[i, 3].Value).ToString();
+                if (s.Contains(selectedValueInCB))
+                {
+                    serviceRequests += (xlApp.Cells[i, 2].Value).ToString() + ", ";
+                }
+            }
+            serviceRequests = serviceRequests.Remove(serviceRequests.LastIndexOf(','));
+        }
+        private void ButtonForCalculatingCATS_Click(object sender, EventArgs e)
+        {
+            int calculatedTime = 0;
+            for (int i = 2; i <= lastRow; i++)
+            {
+                string s = (xlApp.Cells[i, 3].Value).ToString();
+                if (s.Contains(selectedValueInCB))
+                {
+                    calculatedTime += Convert.ToInt32(xlApp.Cells[i, 6].Value);
+                }
+            }
+            LabelTitleForCalcTime.Visible = true;
+            LabelForCalculatedTime.Text = Math.Round((((float)calculatedTime) / 60), 2).ToString();
+        }
+        private void ComboBoxForDates_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedValueInCB = dates[ComboBoxForDates.SelectedIndex];
+        }
+
+        private void ButtonForCalculatingCATSweek_Click(object sender, EventArgs e)
+        {
+            time.Clear();
+            CalculateTime();
+            LabelTitleForCalcTime.Visible = true;
+            LabelForCalculatedTime.Text = Math.Round((((float)time.Sum()) / 60), 2).ToString();
+        }
+
+        private void LabelForCalculatedTime_Click(object sender, EventArgs e)
+        {
+            if (LabelForCalculatedTime.Text.Length > 0)
+            {
+                Clipboard.SetText(LabelForCalculatedTime.Text, TextDataFormat.UnicodeText);
+            }
+        }
+        private void ButtonForServiceRequest_Click(object sender, EventArgs e)
+        {
+            GetServiceRequests();
+            if (serviceRequests.Length > 0)
+            {
+                Clipboard.SetText(serviceRequests, TextDataFormat.UnicodeText);
+            }
+            else
+            {
+                MessageBox.Show("Обращения отсутствуют");
+            }
+        }
+
+        private void LabelForCalculatedTime_MouseEnter(object sender, EventArgs e)
+        {
+            LabelForCalculatedTime.Cursor = Cursors.Hand;
+        }
+    }
+}
